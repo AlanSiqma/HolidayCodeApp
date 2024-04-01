ad1c0a6c93c1075a9a7f7d4364494c4d28af7d64
Alan S. Maia <alan.siqma@gmail.com>
2024-04-01 12:57:53 -0300

    fix add yml

--- HolidayCodeApp/obj/Debug/net7.0/HolidayCodeApp.AssemblyInfo.cs
+++ HolidayCodeApp/obj/Debug/net7.0/HolidayCodeApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HolidayCodeApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+961cb0671226932323311c79a4110bdfee65ecf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HolidayCodeApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HolidayCodeApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
